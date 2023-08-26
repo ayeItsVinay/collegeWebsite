4d889d91772c1ff5b3c125ff59217cc8bade306a
ayeItsVinay <69321432+ayeItsVinay@users.noreply.github.com>
2023-08-26 17:59:25 +0530

    Add files via upload

---
+++ formHandler.php
@@ -0,0 +1,25 @@
+<?php 
+$name = $_POST['name'];
+$visitorEmail = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$emailFrom = 'myCollege.com/contact';
+
+$emailSubject = 'New Form Submission';
+
+$emailBody = "User Name: $name.\n".
+                "User Email: $visitiorEmail.\n".
+                "Subject: $subject.\n".
+                "Message: $message.\n";
+
+$to = "vinaayy399@gmail.com"
+
+$headers = "From : $emailFrom \r\n";
+$headers .= "Reply To : $visitorEmail \r\n";
+
+mail($to,$emailSubject,$emailBody,$headers);
+
+header("Location: contact.html")
+
+?>
